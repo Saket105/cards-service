a53a5c4aac137734e6ab22fa5ecb2893d9d19c03
saket105 <saketbsc123@gmail.com>
2025-09-06 16:42:03 +0530

    initial commit

---
+++ src/test/java/com/saket/eazybytes/cards/CardsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saket.eazybytes.cards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
